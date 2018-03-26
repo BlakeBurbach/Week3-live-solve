7ffe5646f45b4171ffd461faa36502da16d5f582
Blake Burbach <cras.bburbach@gmail.com>
2018-03-26 09:23:35 -0500

    setup server js

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 6590;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); 
+
+app.use(express.static('server/static'));
+
+app.listen(PORT, (req, res)=>{
+    console.log('Listening on port', PORT);
+});
